38250c9ec5ed5032859e8fda6d1dddc09230db5a
Bumsik <Bumsik@Lyon-pc>
2017-03-25 19:17:28 +0900

    thymeleaf template apply

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,13 +1,15 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DemoController {
 	
 	@RequestMapping(value = "/")
-	public String demo(){
+	public String demo(Model model){
+		model.addAttribute("name", "Hello springboot!!!!");
 		return "demo/hello";
 	}
 	
